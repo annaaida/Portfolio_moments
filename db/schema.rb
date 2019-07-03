642f22cc462699d20d0337e64ae25e0ebdc95986
annaaida <annatakahashi813@gmail.com>
2019-07-03 06:28:21 +0000

    [Add] admin and photographer devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_045111) do
+ActiveRecord::Schema.define(version: 2019_07_03_052733) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
@@ -73,6 +90,36 @@ ActiveRecord::Schema.define(version: 2019_07_03_045111) do
     t.string "image_6"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "photographers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "kana_first_name"
+    t.string "kana_last_name"
+    t.string "tel"
+    t.string "profile_img"
+    t.string "price"
+    t.string "area"
+    t.string "mother_tongue"
+    t.string "language_1"
+    t.string "language_2"
+    t.text "introduction"
+    t.boolean "is_deleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_photographers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_photographers_on_reset_password_token", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
