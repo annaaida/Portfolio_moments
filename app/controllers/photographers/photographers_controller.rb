b8ff1cebd79ce9b7d8b5c8274d877378efaef07e
annaaida <annatakahashi813@gmail.com>
2019-07-04 04:30:11 +0000

    [Add] Photographer's mypage top(index)

--- app/controllers/photographers/photographers_controller.rb
+++ app/controllers/photographers/photographers_controller.rb
@@ -1,11 +1,21 @@
 class Photographers::PhotographersController < ApplicationController
+
   def index
+
+  	@photographer = current_photographer
+
   end
 
   def show
+
+  	@photographer = current_photographer
+
   end
 
   def edit
+
+  	@photographer = current_photographer
+
   end
 
   def update
