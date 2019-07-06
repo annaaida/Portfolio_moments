a589d2737af9ac4e60076dc0ad285693e8bf42d0
annaaida <annatakahashi813@gmail.com>
2019-07-06 11:59:15 +0000

    [Add] header jQuery

---
+++ app/assets/javascripts/users/users.js
@@ -0,0 +1,15 @@
+
+$(function() {
+  var $win = $(window),
+      $header = $('header'),
+      animationClass = 'is-animation';
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > 100 ) {
+      $header.addClass(animationClass);
+    } else {
+      $header.removeClass(animationClass);
+    }
+  });
+});
