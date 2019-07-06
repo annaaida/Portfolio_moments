c671714268de79499216183d94c7ce3a9b3da628
annaaida <annatakahashi813@gmail.com>
2019-07-06 09:14:03 +0000

    [Reconstruct] Photographer's index and show page

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,2 +1,17 @@
 class Photographer < ApplicationRecord
+
+  has_many :images, dependent: :destroy, inverse_of: :photographer
+  accepts_nested_attributes_for :images, allow_destroy: true
+
+
+  attachment :profile_img
+
+
+ # フルネームを表示するためメソッドを定義
+
+
+  def full_name
+    self.last_name + self.first_name
+  end
+
 end
