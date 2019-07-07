d69d9b4f385b6401a39751b5958b186dc1f82f2f
annaaida <annatakahashi813@gmail.com>
2019-07-07 06:32:06 +0000

    [Add] User_id column to Photographer's table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_085652) do
+ActiveRecord::Schema.define(version: 2019_07_07_063013) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_085652) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
