de81df303d67ea7515f999c7bc9dd82d022abe54
annaaida <annatakahashi813@gmail.com>
2019-07-08 04:16:26 +0000

    [Edit] photographer's model

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -3,7 +3,6 @@ class Photographer < ApplicationRecord
   has_many :images, dependent: :destroy, inverse_of: :photographer
   accepts_nested_attributes_for :images, allow_destroy: true
 
-
   attachment :profile_img
 
  # フルネームを表示するためメソッドを定義
