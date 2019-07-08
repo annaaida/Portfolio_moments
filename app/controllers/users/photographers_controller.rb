1be55e508e868782f1a2d6ab4d90e1a3117a19e3
annaaida <annatakahashi813@gmail.com>
2019-07-08 11:03:02 +0000

    [Add] photographer's show page half-way

--- app/controllers/users/photographers_controller.rb
+++ app/controllers/users/photographers_controller.rb
@@ -1,7 +1,21 @@
 class Users::PhotographersController < ApplicationController
+
   def index
   end
 
   def show
+
+  	@user = User.find(params[:id])
+  	@photographer = Photographer.find(params[:id])
+
   end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :kana_first_name, :kana_last_name)
+  end
+
+  def photographer_params
+    params.require(:photographer).permit(:profile_img, :price, :area, :mother_tongue, :language_1, :language_2, :introduction)
+  end
+
 end
