077e7f6ab286550aa1343ba6d492d46d2dbae3ea
annaaida <annatakahashi813@gmail.com>
2019-07-09 11:38:56 +0000

    [Fix] config/routes

--- app/controllers/users/books_controller.rb
+++ app/controllers/users/books_controller.rb
@@ -1,9 +1,22 @@
 class Users::BooksController < ApplicationController
 
   def new
+
+  	book = Book.new
+  	book.user_id = current_user.id
+
   end
 
   def create
+
+  	book.save
+
+  end
+
+  private
+
+  def book_params
+  	params.require(:cart).permit(:date, :time, :message, :meeting_spot, :total_price)
   end
 
 end
