8e1a4eb16e90e168760b2de379e15e86ff1d1394
annaaida <annatakahashi813@gmail.com>
2019-07-09 10:29:41 +0000

    [Add] photographer_id colomn to users table as a foreign key

--- app/controllers/users/cities_controller.rb
+++ app/controllers/users/cities_controller.rb
@@ -14,12 +14,4 @@ class Users::CitiesController < ApplicationController
   	params.require(:city).permit(:city_name, :city_img, :about_city)
   end
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :kana_first_name, :kana_last_name)
-  end
-
-  def photographer_params
-    params.require(:photographer).permit(:profile_img, :price, :area, :mother_tongue, :language_1, :language_2, :introduction, :city_id)
-  end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_051731) do
+ActiveRecord::Schema.define(version: 2019_07_09_102752) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_051731) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "photographer"
+    t.integer "photographer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
