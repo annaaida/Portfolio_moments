6e6f2b3358340459d7d63185d59db6b5173adb4e
annaaida <annatakahashi813@gmail.com>
2019-07-13 11:27:26 +0000

    [Add] more user's info in users/index

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,38 +1,46 @@
 class Admins::UsersController < ApplicationController
 
   def index
+
     @users = User.page(params[:page]).per(50)
+
   end
 
   def show
+
     @user = User.find(params[:id])
-    @orders = @user.orders.page(params[:page]).per(10)
-    @reviews = @user.reviews
+
   end
 
   def edit
+
     @user = User.find(params[:id])
+
   end
 
   def update
+
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_user_path(@user), notice: "編集しました"
+      redirect_to admins_user_path(@user), notice: "編集しました"
     else
       render :template => "admin/users/edit"
     end
+
   end
 
   def destroy
+
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path, notice: "削除しました"
+    redirect_to admins_users_path, notice: "削除しました"
+
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code,
-                               :address, :tel, :email)
+    params.require(:user).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :email)
+  end
 
 end
