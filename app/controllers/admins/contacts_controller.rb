583794b7a13c12e87559fed4c4d4c2d43f143b82
annaaida <annatakahashi813@gmail.com>
2019-07-13 10:01:07 +0000

    [Add] admin's contact index page

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -1,16 +1,34 @@
 class Admins::ContactsController < ApplicationController
+
   def top
   end
 
   def index
+    @contacts = Contact.page(params[:page])
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
-  def new
+  def update
+    contact = Contact.find(params[:id])
+    contact.update(contact_params)
+    user = contact.user
+    ContactMailer.send_when_admin_reply(user, contact).deliver
+
+    redirect_to admin_contacts_path
+
   end
 
-  def create
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+    redirect_to admin_contacts_path
   end
+
+  def contact_params
+    params.require(:contact).permit(:reply_text)
+  end
+
 end
