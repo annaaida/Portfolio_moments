bac61e5e691d6666494bcf70dc727f5f5606ad07
annaaida <annatakahashi813@gmail.com>
2019-07-15 15:52:24 +0000

    [Add] jQuery to header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,18 @@
 //= require pickdate/picker.date
 //= require pickdate/picker.time
 //= require_tree .
+
+$(function() {
+  var $win = $(window),
+      $header = $('header'),
+      animationClass = 'transform';
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > 380 ) {
+      $header.addClass(animationClass);
+    } else {
+      $header.removeClass(animationClass);
+    }
+  });
+});
