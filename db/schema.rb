fc952f1d331a6a91e83b9bf0202b54f736821104
annaaida <annatakahashi813@gmail.com>
2019-07-18 00:42:14 +0000

    [Removed] profile_img column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_035538) do
+ActiveRecord::Schema.define(version: 2019_07_18_004109) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -145,7 +145,6 @@ ActiveRecord::Schema.define(version: 2019_07_15_035538) do
     t.datetime "updated_at", null: false
     t.boolean "photographer"
     t.integer "photographer_id"
-    t.string "profile_img"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
