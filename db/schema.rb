e90e14ef5a520274aabe4e76df39848b1d945fe1
annaaida <annatakahashi813@gmail.com>
2019-07-20 08:25:32 +0000

    [Remove] people column from books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_061015) do
+ActiveRecord::Schema.define(version: 2019_07_20_082448) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_07_20_061015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "contact_number"
-    t.integer "people"
     t.time "start_time"
     t.time "end_time"
   end
