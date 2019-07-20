d31bf2ddfeec1af3e959151f3fdf67d917212658
annaaida <annatakahashi813@gmail.com>
2019-07-20 06:04:16 +0000

    [Add] users favorites index

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -2,6 +2,12 @@ class Users::FavoritesController < ApplicationController
 
   def index
 
+  	@favorites = Favorite.page(params[:page]).per(20).order(id: "DESC")
+
+  end
+
+  def favorite_params
+  	params.require(:favorite).permit(:photographer_id, :user_id)
   end
 
 end
