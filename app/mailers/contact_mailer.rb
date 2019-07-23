feddfab4761018633fc4dcf4f11e2352d24b0136
annaaida <annatakahashi813@gmail.com>
2019-07-23 02:30:56 +0000

    [Add] contact mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ApplicationMailer
+
+　default from: 'MOMENTS 管理人 <annaaidamailer@gmail.com>'
+  layout 'mailer'
+
+  def send_when_admin_reply(user, contact)
+  	@user = user
+  	@answer = contact.reply_text
+	mail to: @user.email, subject:'【 MOMENTS 】お問い合わせの回答。'
+  end
+
+end
