70f46b07a658b1b3719589c469f1642784eeaed6
annaaida <annatakahashi813@gmail.com>
2019-07-27 08:56:34 +0000

    [Modify] photographer new page

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eb6e2d9717bc77d22765a15bc8d0febc140857a29e469e1c510d3b93f31df3df1756b535dc390b19edd1e3e6698e9fc18c287da3c13d26e4a42fbc83d66dfcab'
